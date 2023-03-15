ffd43f73d28934c4113b90c3556047af89e51e1d
pmmills <127619228+pmmills@users.noreply.github.com>
2023-03-15 12:07:54 +0000

    Update main.py
    started again

--- main.py
+++ main.py
@@ -1,51 +1,53 @@
+import urllib.parse
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
-# define the URLs for the different locations
-urls = {
-    "London": "https://www.google.com/search?q=weather+London",
-    "New York": "https://www.google.com/search?q=weather+New+York",
-    "Bangkok": "https://www.google.com/search?q=weather+Bangkok",
-    "Lisbon": "https://www.google.com/search?q=weather+Lisbon"
-}
-urlTime = "https://www.google.com/search?q=time+and+date"
+#create list and base url string
+locations = ["London","New+York","Bangkok"]
+base_url = "https://www.google.com/search?q=weather+"
 
-# define header to mimic browser
+#initial variables
+location_name = 'Coventry'
+weather = 'hot'
+celsius = float(99)
+farenheit = float (100)
+
+
+#header used to mimic a browser to pass the info
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"}
 
-for city, url in urls.items():
-    # send a get request to url with headers
-    res = requests.get(url, headers=headers)
+#loop to create dynamic url for the web pages
+for locations in locations:
+    #generate the url for locaion
+    url = base_url + locations.replace(" ", "+")
 
-    # parse the html though beautifulsoup
-    soup = BeautifulSoup(res.text, 'html.parser')
+    #send get request with the header to act as a browser and bring back the info
+    response = requests.get(url, headers=headers)
 
-    # find elements using css selector
-    location = soup.select("span.BBwThe")[0].getText()
+    #parse the information with the beautifulsoup library
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Extract current weather information
+    location_name = soup.select("span.BBwThe")[0].getText()
     time = soup.select("#wob_dts")[0].getText().strip()
     info = soup.select("#wob_dc")[0].getText().strip()
     temperature = soup.select("#wob_tm")[0].getText().strip()
 
-    # output the weather information for the current city
-    print(f"{city} - {location}")
+    farenheit = celsius * 9/5 + 32
+
+    # Print weather information for location
+    #print(url)
+
+    print(f"Location: {location_name}")
     print(f"Time: {time}")
-    print(f"Info: {info}")
-    print(f"Temperature: {temperature}°C")
-    print("\n")
+    print(f"Weather: {info}")
+    print(f"Temperature: {celsius}°C, {farenheit}°F\n")
 
-# send a get request to time and date URL with headers
-resTime = requests.get(urlTime, headers=headers)
+#time format for lewis
+now =  datetime.now()
+lewis_dt = now.strftime("%d_%m_%y - %H:%M:%S")
+print(f"-----------------\n")
+print(f"Current Date - Time =", lewis_dt)
 
-# parse the html though beautifulsoup
-soup1 = BeautifulSoup(resTime.text, 'html.parser')
-
-# Find the current time element using its CSS selector and get its text content
-time_element = soup1.select('div.gsrt.vk_bk.FzvWSb.YwPhnf[aria-level="3"]')[0]
-current_time = time_element.getText()
-date_element = soup1.select("span.KfQeJ")[0].getText()
-
-# output the current time and date
-print("Current date:", date_element)
-print("Current time:", current_time)
-print("Based on UK Timezone")
